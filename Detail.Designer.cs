ec96484b38d719b127e3a71f60b8ae85db4f56d6
ijnam7 <ijnam7@gmail.com>
2023-11-27 08:21:46 +0900

    Designed layout

--- Detail.Designer.cs
+++ Detail.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HaeyaCalendar
 {
-    partial class Form1
+    partial class Detail
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(343, 304);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Detail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(367, 344);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Detail";
+            this.Text = "Detail";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
-
